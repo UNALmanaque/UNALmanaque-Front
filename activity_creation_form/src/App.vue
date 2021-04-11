8acae81dd510eaa350fcbcbe6fc07f3dc0d1928f
mmedranom <mmedranom@unal.edu.co>
2021-04-11 15:04:35 -0500

    Activiry Component template

--- activity_creation_form/src/App.vue
+++ activity_creation_form/src/App.vue
@@ -1,19 +1,23 @@
 <template>
-  <v-app>
+  <v-app> <!-- Todo lo que se quiera trabajar con Vuetify se pone aqui adentro en v-app -->
     <Activity_Form></Activity_Form>
+
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+
 import Activity_Form from './components/Activity_Form';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
-    Activity_Form
+    Activity_Form,
   },
 
   data: () => ({
--- activity_creation_form/src/components/Activity_Form.vue
+++ activity_creation_form/src/components/Activity_Form.vue
@@ -1,7 +1,15 @@
 <template>
-    <div>
+    <container class="grey lighten-5">
+        <v-row>
+            <v-col cols="12" xl="4">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt sequi aut suscipit, fuga sint distinctio sunt inventore natus officia, debitis eligendi possimus quas culpa accusantium, quibusdam magni reprehenderit explicabo? Quis!</p>
+            </v-col>
+            <v-col cols="12" xl="8">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt sequi aut suscipit, fuga sint distinctio sunt inventore natus officia, debitis eligendi possimus quas culpa accusantium, quibusdam magni reprehenderit explicabo? Quis!</p>
+            </v-col>
+        </v-row>
         <h1>Activity</h1>
-    </div>
+    </container>
 </template>
 
 <script>
