6eaa63d2ca04bf4e57dcfa0d6547c3799c472dc4
lgordillom <lgordillom@unal.edu.co>
2021-04-28 19:33:44 -0500

    borrar y listar

--- src/views/Activities/Activities.js
+++ src/views/Activities/Activities.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import { mapActions, mapGetters } from 'vuex';
+import firebase from 'firebase';
 
 export default {
   data() {
     return {
-      activities: []
+      activities: [],
+      id: 10
     };
   },
   created() {
@@ -16,17 +18,33 @@ export default {
   methods: {
     ...mapActions(['setUser']),
     getActivities() {
+      //var userId;
+      const user =firebase.auth().currentUser;
+      if(user){
+
+      axios.get('/api/user/find/'+user.email)
+      .then(response => {
+        console.log(response.data.userId)
+        this.id = response.data.userId;
+        
+      });
+    
+    
+      console.log("holaaa", this.id)
       axios
-        .get('api/activities/')
+        .get('/api/event/find/'+this.id)
         .then((res) => {
           //iterates over the response adding each
           //activity to the array
+          console.log(res)
           res.data.forEach(element => {
-            element.repeticion.toString()
+            console.log(element)
+            element.eventDaily.toString()
             this.activities.push(element);
           });
         })
         .catch((err) => {
+          console.log(err)
           this.$toast.error(err.response.data, { position: 'top-right' });
           setTimeout(this.$toast.clear, 3000);
           //DELETE THIS CODE AFTER GET IS WORKING
@@ -46,10 +64,19 @@ export default {
           }
           /////////
         });
+      }
     },
     deleteActivity(id){
       //Here should go a post method
       console.log('activity to delete: ', id);
+      axios.delete('/api/event/delete/'+id)
+      .then( res => {
+        this.$toast.info(`Borrado exitoso`, { position: 'top-right' });
+        console.log(res)
+        this.activities= []
+        this.getActivities()
+        
+      });
       //call the get activity again
       //getActivities()
     },
